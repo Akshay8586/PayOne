541864659e4c2727547c1f2b4a6de9ac3e540b1a
Akshay8586 <akshaydhandhi8@gmail.com>
2024-01-30 00:55:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://pay-dn1v6zw04-akshay-kumars-projects-5a1eec33.vercel.app"],
+    methods: ["POST","GET","PUT"],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
