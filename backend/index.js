2d7d7d5979d9a66ff8805c057e43a3794991a120
Akshay8586 <akshaydhandhi8@gmail.com>
2024-01-30 01:25:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header({"Access-Control-Allow-Origin": "*"});
+    next();
+  }) 
+
 app.use("/api/v1", rootRouter);
 
 app.listen(3000);
